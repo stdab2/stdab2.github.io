74aa801f53e4dff5d8bad6f1e87fea8d83b61fd5
Steeve Dabire <steeve.dabire.1@ulaval.ca>
2023-12-05 18:35:36 -0500

    Second

---
+++ LE_TP2/PersonneException.h
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
+ */
+
+/* 
+ * File:   PersonneException.h
+ * Author: etudiant
+ *
+ * Created on 5 décembre 2023, 18 h 31
+ */
+
+#ifndef PERSONNEEXCEPTION_H
+#define PERSONNEEXCEPTION_H
+
+#include <stdexcept>
+
+class PersonneException : public std::runtime_error{
+public:
+	PersonneException(const std::string& p_raison): std::runtime_error(p_raison){}
+};
+
+class PersonneDejaPresenteException: public PersonneException{
+public:
+	PersonneDejaPresenteException(const std::string& p_raison): PersonneException(p_raison){}
+};
+
+class PersonneNonPresenteException: public PersonneException{
+public:
+	PersonneNonPresenteException(const std::string& p_raison): PersonneException(p_raison){}
+};
+
+#endif /* PERSONNEEXCEPTION_H */
+
